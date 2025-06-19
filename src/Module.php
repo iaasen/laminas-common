bb777d08cfda1d023a212a5b99b05a94964eef1a
ingvar <ingvar.aasen@nte.no>
2025-06-19 15:05:18 +0200

    Add version 1 to the components iaasen/* and nteb/*

--- src/Module.php
+++ src/Module.php
@@ -8,7 +8,7 @@ use Laminas\ModuleManager\Feature\ConfigProviderInterface;
  */
 class Module implements ConfigProviderInterface
 {
-	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
-	}
+    public function getConfig() {
+        return include __DIR__ . '/../config/module.config.php';
+    }
 }
