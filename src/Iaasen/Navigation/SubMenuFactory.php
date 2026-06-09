c64bdbf976877de0013268f0a6c34b4d704eceea
Ingvar Aasen <ingvar.aasen@nte.no>
2026-06-09 09:45:56 +0200

    Update the MainMenuFactory.php and SubMenuFactory.php

--- src/Iaasen/Navigation/SubMenuFactory.php
+++ src/Iaasen/Navigation/SubMenuFactory.php
@@ -2,33 +2,27 @@
 /**
  * User: Ingvar
  * Date: 30.04.2016
- * Time: 06.01
  */
-
 namespace Iaasen\Navigation;
 
-
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
-use Laminas\ServiceManager\ServiceManager;
+use Laminas\View\Helper\Navigation\Menu;
 
 class SubMenuFactory implements FactoryInterface
 {
 	/**
-	 * @param ServiceManager $container
 	 * @param string $requestedName
-	 * @param array|null $options
-	 * @return \Laminas\View\Helper\Navigation\Menu|object
+	 * @return Menu
 	 */
-	public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+	public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
 	{
-		$menu = new \Laminas\View\Helper\Navigation\Menu();
+		$menu = new Menu();
 		$menu->setUlClass('nav nav-pills nav-stacked');
 		$menu->setOnlyActiveBranch(true);
 		$menu->setRenderParents(false);
 		$menu->setMinDepth(1);
 		$menu->setMaxDepth(2);
-		//$menu->setPartial(['layout/menuPartial', 'settings']);
 		return $menu;
 	}
 }
